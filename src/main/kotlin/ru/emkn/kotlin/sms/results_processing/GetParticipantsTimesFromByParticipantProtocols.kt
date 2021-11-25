86d7ce1e7c2b3adb9d349ca5559ef440fc45d67c
James <js7572305@gmail.com>
2021-11-25 20:39:04 +0300

    Added documentation in result processing package

--- src/main/kotlin/ru/emkn/kotlin/sms/results_processing/GetParticipantsTimesFromByParticipantProtocols.kt
+++ src/main/kotlin/ru/emkn/kotlin/sms/results_processing/GetParticipantsTimesFromByParticipantProtocols.kt
@@ -3,7 +3,14 @@ package ru.emkn.kotlin.sms.results_processing
 import ru.emkn.kotlin.sms.Route
 import ru.emkn.kotlin.sms.time.Time
 
-
+/**
+ * @param routeCompletionProtocols  completion protocols
+ * @param routeGetterFunction  function that maps the participant's id to his route
+ * @param startTimeGetter  function that maps the participant's id to his start time
+ * @return map M such that m.at(participantId) = participantTimeInSeconds and
+ * <code> m.at(participantId) </code> === null if the participant is disqualified. The map M contains
+ * only people mentioned in protocols.
+ */
 fun getParticipantsTimesFromByParticipantProtocols(
     routeCompletionProtocols: List<RouteCompletionByParticipantProtocol>,
     routeGetterFunction: (Int) -> Route,
