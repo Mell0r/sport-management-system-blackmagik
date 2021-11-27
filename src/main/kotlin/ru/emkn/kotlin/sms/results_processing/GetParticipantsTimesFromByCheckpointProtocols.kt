2262490b12d69c88d6bf2dcfddba22774bf97e40
James <js7572305@gmail.com>
2021-11-27 09:53:16 +0300

    Refactored times calculator functions.

--- src/main/kotlin/ru/emkn/kotlin/sms/results_processing/GetParticipantsTimesFromByCheckpointProtocols.kt
+++ src/main/kotlin/ru/emkn/kotlin/sms/results_processing/GetParticipantsTimesFromByCheckpointProtocols.kt
@@ -3,43 +3,70 @@ package ru.emkn.kotlin.sms.results_processing
 import ru.emkn.kotlin.sms.Route
 import ru.emkn.kotlin.sms.time.Time
 
+internal data class IdCheckpointTime(
+    val id: Int, val checkpoint: CheckpointLabelT, val time: Time
+)
+
 /**
  * @param routeCompletionProtocols  completion protocols
- * @param routeGetterFunction  function that maps the participant's id to his route
+ * @param idToRoute  function that maps the participant's id to his route
  * @param startTimeGetter  function that maps the participant's id to his start time
  * @return map M such that m.at(participantId) = participantTimeInSeconds and
  * <code> m.at(participantId) </code> === null if the participant is disqualified. The map M contains
  * only people mentioned in protocols.
  */
+
 fun getParticipantsTimesFromByCheckpointProtocols(
     routeCompletionProtocols: List<RouteCompletionByCheckpointProtocol>,
-    routeGetterFunction: (Int) -> Route,
+    idToRoute: (Int) -> Route,
     startTimeGetter: (Int) -> Time
 ): Map<Int, Int?> {
-    val listOfIdCheckpointAndTime =
+    val listOfIdCheckpointTimes =
         routeCompletionProtocols.flatMap { protocol ->
             val checkpointLabel = protocol.checkpointLabel
             protocol.participantTimes.map { (id, time) ->
-                Triple(id, checkpointLabel, time)
+                IdCheckpointTime(id, checkpointLabel, time)
             }
         }
 
-    val groupedById = listOfIdCheckpointAndTime.groupBy({ it.first }) {
-        RouteCompletionByParticipantEntry(
-            it.second,
-            it.third
-        )
+    return processIdCheckpointTimeList(
+        listOfIdCheckpointTimes,
+        startTimeGetter,
+        idToRoute
+    )
+}
+
+internal fun processIdCheckpointTimeList(
+    listOfIdCheckpointAndTime: List<IdCheckpointTime>,
+    startTimeGetter: (Int) -> Time,
+    idToRoute: (Int) -> Route
+): Map<Int, Int?> {
+    val groupedById = listOfIdCheckpointAndTime.groupBy({ it.id }) {
+        RouteCompletionByParticipantEntry(it.checkpoint, it.time)
     }
     return groupedById.map { (id, checkpointsToTimes) ->
-        val sortedCheckpointsToTimes = checkpointsToTimes.sortedBy { it.time }
-        if (sortedCheckpointsToTimes.map{it.checkpointLabel} != routeGetterFunction(id).route)
-            Pair(id, null)
-        else {
-            Pair(
-                id,
-                sortedCheckpointsToTimes.last().time - startTimeGetter(id)
-            )
-        }
+        generateIdToResultsPair(
+            id, checkpointsToTimes, startTimeGetter, idToRoute
+        )
     }.toMap()
+}
+
+private fun generateIdToResultsPair(
+    id: Int,
+    checkpointsToTimes: List<RouteCompletionByParticipantEntry>,
+    startTimeGetter: (Int) -> Time,
+    idToRoute: (Int) -> Route
+): Pair<Int, Int?> {
+    val checkpointsToTimesChronological =
+        checkpointsToTimes.sortedBy { it.time }
+    val chronologicalCheckpoints =
+        checkpointsToTimesChronological.map { it.checkpointLabel }
+    return if (chronologicalCheckpoints != idToRoute(id).route)
+        Pair(id, null)
+    else {
+        val finishTime = checkpointsToTimesChronological.last().time
+        val timeForDistance = finishTime - startTimeGetter(id)
+        Pair(id, timeForDistance)
+    }
 }
 
--- src/main/kotlin/ru/emkn/kotlin/sms/results_processing/GetParticipantsTimesFromByParticipantProtocols.kt
+++ src/main/kotlin/ru/emkn/kotlin/sms/results_processing/GetParticipantsTimesFromByParticipantProtocols.kt
@@ -5,7 +5,7 @@ import ru.emkn.kotlin.sms.time.Time
 
 /**
  * @param routeCompletionProtocols  completion protocols
- * @param routeGetterFunction  function that maps the participant's id to his route
+ * @param idToRoute  function that maps the participant's id to his route
  * @param startTimeGetter  function that maps the participant's id to his start time
  * @return map M such that m.at(participantId) = participantTimeInSeconds and
  * <code> m.at(participantId) </code> === null if the participant is disqualified. The map M contains
@@ -13,23 +13,22 @@ import ru.emkn.kotlin.sms.time.Time
  */
 fun getParticipantsTimesFromByParticipantProtocols(
     routeCompletionProtocols: List<RouteCompletionByParticipantProtocol>,
-    routeGetterFunction: (Int) -> Route,
+    idToRoute: (Int) -> Route,
     startTimeGetter: (Int) -> Time
 ): Map<Int, Int?> {
-    val results = routeCompletionProtocols.map { protocol ->
-        val route = routeGetterFunction(protocol.id)
-        val checkpointTimes = protocol.checkpointTimes.sortedBy { entry ->
-            entry.time
+
+    val listOfIdCheckpointTimes =
+        routeCompletionProtocols.flatMap { protocol ->
+            val id = protocol.id
+            protocol.checkpointTimes.map { (checkpointLabel, time) ->
+                IdCheckpointTime(id, checkpointLabel, time)
+            }
         }
-        if (checkpointTimes.map { it.checkpointLabel } != route.route)
-            Pair(protocol.id, null)
-        else {
-            Pair(
-                protocol.id,
-                checkpointTimes.last().time - startTimeGetter(protocol.id)
-            )
-        }
-    }
-    return results.toMap()
+
+    return processIdCheckpointTimeList(
+        listOfIdCheckpointTimes,
+        startTimeGetter,
+        idToRoute
+    )
 }
 
