c06bbc64d6b876fe33c7e5ec93ab42c71bc0ecdd
Данила <duesna897@gmail.com>
2021-11-27 18:35:40 +0300

    Main.kt draft

--- src/main/kotlin/ru/emkn/kotlin/sms/Main.kt
+++ src/main/kotlin/ru/emkn/kotlin/sms/Main.kt
@@ -2,6 +2,8 @@ package ru.emkn.kotlin.sms
 
 import org.tinylog.kotlin.Logger
 import ru.emkn.kotlin.sms.cli.*
+import ru.emkn.kotlin.sms.io.*
+import kotlin.system.exitProcess
 
 /**
  * All possible program modes.
@@ -17,6 +19,28 @@ fun main(args: Array<String>) {
 
     val argParsingSystem = ArgParsingSystem()
     argParsingSystem.parse(args)
+
+    //val competition: Competition = TODO()
+
+    val outputDirectory = ensureDirectory(argParsingSystem.outputDirectory.absolutePath) ?: {
+        Logger.error {"Couldn't set up output directory. Terminating."}
+        exitProcess(-1)
+    }
+
+    when (argParsingSystem.invokedSubcommand) {
+        ProgramSubcommands.START -> {
+            val applications = readAllReadableFiles(argParsingSystem.startCommand.applicationFiles) {
+                Logger.warn {"Couldn't reach or read application \"$it\"."}
+            }
+            TODO()
+        }
+        ProgramSubcommands.RESULT -> TODO()
+        ProgramSubcommands.RESULT_TEAMS -> TODO()
+        null -> {
+            Logger.error {"No command was invoked. Terminating."}
+            exitProcess(-1)
+        }
+    }
 
     Logger.debug { "Program successfully finished." }
 }
