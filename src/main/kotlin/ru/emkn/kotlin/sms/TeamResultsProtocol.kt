15c2b52c9aacb5b02c301066b09018186d7ed8b8
James <js7572305@gmail.com>
2021-11-28 17:03:06 +0300

    Implemented generateTeamResultsProtocol and TeamResultsProtocol::dumpToCsv

--- src/main/kotlin/ru/emkn/kotlin/sms/TeamResultsProtocol.kt
+++ src/main/kotlin/ru/emkn/kotlin/sms/TeamResultsProtocol.kt
@@ -2,8 +2,24 @@ package ru.emkn.kotlin.sms
 
 import ru.emkn.kotlin.sms.results_processing.FileContent
 
-class TeamResultsProtocol : CsvDumpable {
+data class TeamToScore(val team: String, val score: Int)
+class TeamResultsProtocol(scores: List<TeamToScore>) : CsvDumpable {
+    private val scores = scores.sortedBy { it.team }.sortedBy { it.score }
+    private val places = generatePlaces()
+
+    private fun generatePlaces(): List<Int> {
+        val places = (1..scores.size).toMutableList()
+        for (i in 0 until scores.lastIndex) {
+            if (scores[i].score == scores[i + 1].score)
+                places[i + 1] = places[i]
+        }
+        return places
+    }
+
     override fun dumpToCsv(): FileContent {
-        TODO("Not yet implemented")
+        val header = listOf("Место,Команда,Баллы")
+        val rest =
+            scores.zip(places) { (team, score), place -> "$place,$team,$score" }
+        return header + rest
     }
 }
---
+++ src/main/kotlin/ru/emkn/kotlin/sms/results_processing/GenerateTeamResultsProtocol.kt
@@ -0,0 +1,58 @@
+package ru.emkn.kotlin.sms.results_processing
+
+import ru.emkn.kotlin.sms.*
+import kotlin.math.max
+
+// all the functions below should be NOT in this file, but in respective module.
+// competition parameter is everywhere just in case; worst case scenario:
+// it will be deleted as unused parameter later.
+fun generateTeamResultsProtocol(
+    groupResultProtocols: List<GroupResultProtocol>,
+    participantsList: ParticipantsList,
+    competitionConfig: Competition
+): TeamResultsProtocol {
+    val idToScore = HashMap<Int, Int>()
+    groupResultProtocols.forEach { groupResultProtocol ->
+        moveCalculatedScoresIntoMap(groupResultProtocol, idToScore)
+    }
+    val teamsToScore = groupResultProtocols
+        .flatMap { it.entries }
+        .groupBy { it.participant.team }
+        .mapValues { (_, listParticipantAndTime) ->
+            val idsOfTeam = listParticipantAndTime.map { it.participant.id }
+            idsOfTeam.sumOf { idToScore[it]!! }
+        }
+    return TeamResultsProtocol(teamsToScore.entries.map { (team, score) ->
+        TeamToScore(
+            team,
+            score
+        )
+    })
+}
+
+private fun moveCalculatedScoresIntoMap(
+    groupResultProtocol: GroupResultProtocol,
+    idToScore: HashMap<Int, Int>
+) {
+    val bestResult: Int? =
+        groupResultProtocol.entries.mapNotNull { it.totalTime }
+            .minOfOrNull { it.asSeconds() }
+    if (bestResult == null) {
+        groupResultProtocol.entries.map { it.participant.id }
+            .forEach { id ->
+                idToScore[id] = 0
+            }
+    } else {
+        groupResultProtocol.entries.map { it.participant.id to it.totalTime?.asSeconds() }
+            .forEach { (id, totalTime) ->
+                if (totalTime != null) {
+                    idToScore[id] = max(
+                        0,
+                        100 * (2 - totalTime.toFloat() / bestResult.toFloat()).toInt()
+                    )
+                } else {
+                    idToScore[id] = 0
+                }
+            }
+    }
+}
