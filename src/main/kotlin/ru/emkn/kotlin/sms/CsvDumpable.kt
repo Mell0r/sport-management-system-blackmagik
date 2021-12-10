e8879e5f8cc5a4607ce1170869538a8a85806c2f
James <js7572305@gmail.com>
2021-12-10 06:00:20 +0300

    Removed the outdated comment.

--- src/main/kotlin/ru/emkn/kotlin/sms/CsvDumpable.kt
+++ src/main/kotlin/ru/emkn/kotlin/sms/CsvDumpable.kt
@@ -2,27 +2,6 @@ package ru.emkn.kotlin.sms
 
 import ru.emkn.kotlin.sms.results_processing.FileContent
 
-/*
-
-Class requirements:
-o - means 'implements CsvDumpable'
-i - means 'companion object of respected class T contains method
-fromFileContent(fileContent: FileContent): T, that throws Illegal argument exception
-if the format is bad.
-
-i o classname
-
-i   Application
-i o ParticipantList
-i o StartingProtocol
-i   ParticipantTimestampsProtocol
-i   CheckpointTimestampsProtocol
-i o GroupResultProtocol*
-  o TeamResultsProtocol*
-
-* - these classes don't exist, although they should
-
- */
 interface CsvDumpable {
     fun dumpToCsv(): FileContent
 }
