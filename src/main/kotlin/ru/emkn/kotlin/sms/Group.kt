7f30ca8d87d1614056d24937ba32097cdb1e6407
Данила <duesna897@gmail.com>
2021-12-11 19:25:23 +0300

    Fixed Group.toString()

--- src/main/kotlin/ru/emkn/kotlin/sms/Group.kt
+++ src/main/kotlin/ru/emkn/kotlin/sms/Group.kt
@@ -2,6 +2,7 @@ package ru.emkn.kotlin.sms
 
 abstract class Group(val label: String, val route: Route) {
     abstract fun checkParticipantValidity(participant: Participant) : Boolean
+    override fun toString() = label
 }
 
 class AgeGroup(label: String, route: Route, private val ageFrom: Int, private val ageTo: Int) : Group(label, route) {
