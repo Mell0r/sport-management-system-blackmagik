3d7d1cd4218f611470dfafb3decc6a4aeef255e0
Данила <duesna897@gmail.com>
2021-12-14 18:21:06 +0300

    Introduced UniqueListBuilder

--- src/main/kotlin/ru/emkn/kotlin/sms/gui/CompetitionBuilder.kt
+++ src/main/kotlin/ru/emkn/kotlin/sms/gui/CompetitionBuilder.kt
@@ -10,66 +10,40 @@ import ru.emkn.kotlin.sms.Route
  * instance of Competition class.
  */
 class CompetitionBuilder (
-    var discipline: String,
-    var name: String,
-    var year: Int,
-    var date: String,
-    private val groups: MutableList<Group>, // all groups are guaranteed to be unique
-    private val routes: MutableList<Route>, // all routes are guaranteed to be unique
+    var discipline: String = "",
+    var name: String = "",
+    var year: Int = 0,
+    var date: String = "",
 ) {
+    val groupsBuilder = UniqueListBuilder<Group>(
+        equals = { group1, group2 -> group1.label == group2.label }
+    )
+    val routesBuilder = UniqueListBuilder<Route>(
+        equals = { route1, route2 -> route1.name == route2.name }
+    )
+
+    companion object {
+        /**
+         * Creates a [CompetitionBuilder] from [Competition].
+         * Useful for loading competition and then modifying it in GUI.
+         */
+        fun fromCompetition(competition: Competition): CompetitionBuilder {
+            val builder = CompetitionBuilder(
+                discipline = competition.discipline,
+                name = competition.name,
+                year = competition.year,
+                date = competition.date,
+            )
+            builder.groupsBuilder.replaceList(competition.groups)
+            builder.routesBuilder.replaceList(competition.routes)
+            return builder
+        }
+    }
+
     /**
      * Returns built instance of [Competition] class.
      */
     fun build(): Competition {
-        TODO()
-    }
-
-    val competition: Competition
-        get() = build()
-
-    /**
-     * Tries to add a new group.
-     *
-     * @param [group] to be added.
-     *
-     * @throws [IllegalArgumentException] if
-     * a group with the same name already exists
-     * or some other error occurs.
-     */
-    fun addGroup(group: Group) {
-        TODO()
-    }
-
-    /**
-     * Tries to remove a group.
-     * Does nothing if the [group] is not in [groups] list.
-     *
-     * @param [group] to be removed.
-     */
-    fun removeGroup(group: Group) {
-        TODO()
-    }
-
-    /**
-     * Tries to add a new route.
-     *
-     * @param [route] to be added.
-     *
-     * @throws [IllegalArgumentException] if
-     * a route with the same name already exists
-     * or some other error occurs.
-     */
-    fun addRoute(route: Route) {
-        TODO()
-    }
-
-    /**
-     * Tries to remove a route.
-     * Does nothing if the [route] is not in [routes] list.
-     *
-     * @param [route] to be removed.
-     */
-    fun removeRoute(route: Route) {
         TODO()
     }
 }
---
+++ src/main/kotlin/ru/emkn/kotlin/sms/gui/UniqueListBuilder.kt
@@ -0,0 +1,71 @@
+package ru.emkn.kotlin.sms.gui
+
+/**
+ * Builds a list of elements, guaranteed to be unique.
+ */
+class UniqueListBuilder<T>(
+    private val list: MutableList<T> = mutableListOf(),
+    private val equals: (T, T) -> Boolean = { lhs, rhs -> lhs === rhs }, // must form an equivalence relation
+) {
+    companion object {
+        fun <T> checkListUniqueness(
+            list: MutableList<T>,
+            equals: (T, T) -> Boolean = { lhs, rhs -> lhs === rhs }, // must form an equivalence relation
+        ) : Boolean {
+            TODO()
+        }
+
+        /**
+         * Create a builder from the [list].
+         *
+         * @throws [IllegalArgumentException] if the [list] had equal elements.
+         */
+        fun <T> fromListAndEquals(
+            list: MutableList<T>,
+            equals: (T, T) -> Boolean = { lhs, rhs -> lhs === rhs }, // must form an equivalence relation
+        ) : UniqueListBuilder<T> {
+            if (!checkListUniqueness(list, equals)) {
+                throw IllegalArgumentException("Some elements of the list are equal.")
+            }
+            TODO()
+        }
+    }
+
+    /**
+     * Builds the list.
+     */
+    fun build(): List<T> {
+        return list.toMutableList() // creates a copy
+    }
+
+    /**
+     * Replaces this.list with a given [list].
+     * Checks that in [list] all elements are unique.
+     *
+     * @return true, if the replacement was successful,
+     * false if the given [list] had equal elements
+     */
+    fun replaceList(list: List<T>) : Boolean {
+        TODO()
+    }
+
+    /**
+     * Tries to add an element the list,
+     * checks that it is unique to all the others.
+     *
+     * @return true if the element was added successfully, false otherwise
+     */
+    fun add(element: T) : Boolean {
+        TODO()
+    }
+
+    /**
+     * Tries to remove an element from the list.
+     *
+     * @return true if the element was removed successfully,
+     * false in case the element was not present in the list
+     */
+    fun remove(element: T) : Boolean {
+        TODO()
+    }
+}
