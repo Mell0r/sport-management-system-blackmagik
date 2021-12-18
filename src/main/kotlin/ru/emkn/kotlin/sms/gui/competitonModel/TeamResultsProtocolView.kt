70ad54378a20643e7e46912162e8f201a660d047
James <js7572305@gmail.com>
2021-12-18 11:09:19 +0300

    Removed redundant import blocking the compilation.

--- src/main/kotlin/ru/emkn/kotlin/sms/gui/competitonModel/TeamResultsProtocolView.kt
+++ src/main/kotlin/ru/emkn/kotlin/sms/gui/competitonModel/TeamResultsProtocolView.kt
@@ -2,7 +2,6 @@ package ru.emkn.kotlin.sms.gui.competitonModel
 
 import ru.emkn.kotlin.sms.ParticipantCheckpointTime
 import ru.emkn.kotlin.sms.TeamResultsProtocol
-import ru.emkn.kotlin.sms.gui.builders.BuilderListener
 
 class TeamResultsProtocolView : CompetitionModelListener  {
     var protocol: TeamResultsProtocol = TeamResultsProtocol(listOf())
