4b9d7435e3bad3f39f87d762b32d3d9ad4f79439
Mell0r <malovtarasov@gmail.com>
2021-12-19 13:25:36 +0300

    Two dialogs(error and success) implemented

---
+++ src/main/kotlin/ru/emkn/kotlin/sms/gui/frontend/elements/SuccessDialog.kt
@@ -0,0 +1,34 @@
+package ru.emkn.kotlin.sms.gui.frontend.elements
+
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.AlertDialog
+import androidx.compose.material.ExperimentalMaterialApi
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+import ru.emkn.kotlin.sms.gui.frontend.getEmojiByUnicode
+
+val greenCheckMark = getEmojiByUnicode(9989)
+
+@OptIn(ExperimentalMaterialApi::class)
+@Composable
+fun SuccessDialog() {
+    val width = 300.dp
+    val height = 100.dp
+    val visible = remember { mutableStateOf(true) }
+    AnimatedVisibility (visible.value) {
+        AlertDialog(
+            onDismissRequest = { visible.value = false },
+            title = { Text(greenCheckMark) },
+            text = { Text("Всё успошно сохранено!") },
+            modifier = Modifier.size(width, height).border(3.dp, Color.Green, shape = RoundedCornerShape(3.dp)),
+            buttons = {})
+    }
+}
