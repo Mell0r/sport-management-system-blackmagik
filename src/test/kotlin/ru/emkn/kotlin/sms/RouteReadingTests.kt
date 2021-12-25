bdeb76bb3731484033db1bbd2dfde481ab3dc844
metametamoon <js7572305@gmail.com>
2021-12-25 10:17:29 +0300

    Fixed tests to match new API.

--- src/test/kotlin/ru/emkn/kotlin/sms/RouteReadingTests.kt
+++ src/test/kotlin/ru/emkn/kotlin/sms/RouteReadingTests.kt
@@ -1,5 +1,7 @@
 package ru.emkn.kotlin.sms
 
+import com.github.michaelbull.result.Ok
+import com.github.michaelbull.result.unwrap
 import kotlin.test.Test
 import kotlin.test.assertEquals
 import kotlin.test.assertFailsWith
@@ -11,12 +13,12 @@ internal class RouteReadingTests {
     fun `properly reads ordered checkpoints routes`() {
         fun checkRouteLine(routeLine: String) {
             val route = readRouteFromLine(routeLine)
-            assertIs<OrderedCheckpointsRoute>(route)
+            assertIs<Ok<OrderedCheckpointsRoute>>(route)
             assertEquals(
                 listOf("chp1", "chp2", "chp3"),
-                route.orderedCheckpoints
+                route.value.orderedCheckpoints
             )
-            assertEquals("name1", route.name)
+            assertEquals("name1", route.value.name)
         }
         checkRouteLine("\$0\$name1,chp1,chp2,chp3")
         checkRouteLine("name1,chp1,chp2,chp3")
@@ -25,7 +27,10 @@ internal class RouteReadingTests {
     @Test
     fun `Properly writes and reads back ordered checkpoints routes`() {
         fun checkRoute(route: OrderedCheckpointsRoute) {
-            assertEquals(route, readRouteFromLine(route.dumpToCsvString()))
+            assertEquals(
+                route,
+                readRouteFromLine(route.dumpToCsvString()).unwrap()
+            )
         }
         checkRoute(OrderedCheckpointsRoute("orderedRoute1", mutableListOf("c1", "c2", "c3")))
         checkRoute(OrderedCheckpointsRoute("orderedRoute2", mutableListOf("c2", "c1", "c3")))
@@ -37,16 +42,19 @@ internal class RouteReadingTests {
     fun `properly reads at least k checkpoints routes`() {
         val routeLine = ("\$1\$name1,2,chp1,chp2,chp3")
         val route = readRouteFromLine(routeLine)
-        assertIs<AtLeastKCheckpointsRoute>(route)
-        assertEquals(setOf("chp1", "chp2", "chp3"), route.checkpoints)
-        assertEquals("name1", route.name)
-        assertEquals(2, route.threshold)
+        assertIs<Ok<AtLeastKCheckpointsRoute>>(route)
+        assertEquals(setOf("chp1", "chp2", "chp3"), route.value.checkpoints)
+        assertEquals("name1", route.value.name)
+        assertEquals(2, route.value.threshold)
     }
 
     @Test
     fun `Properly writes and reads back at least k checkpoints routes`() {
         fun checkRoute(route: AtLeastKCheckpointsRoute) {
-            assertEquals(route, readRouteFromLine(route.dumpToCsvString()))
+            assertEquals(
+                route,
+                readRouteFromLine(route.dumpToCsvString()).unwrap()
+            )
         }
         checkRoute(AtLeastKCheckpointsRoute("atLeastKRoute1", mutableSetOf("c1", "c2", "c3"), 1))
         checkRoute(AtLeastKCheckpointsRoute("atLeastKRoute2", mutableSetOf("c2", "c1", "c3"), 2))
