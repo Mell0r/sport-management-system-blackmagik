8f3f8f31dee6712c0ae8ae351d0e22a36a8d5b1c
Данила <duesna897@gmail.com>
2021-12-25 14:43:39 +0300

    Introduced ParticipantsListDbWriter

---
+++ src/main/kotlin/ru/emkn/kotlin/sms/db/ParticipantsListDbWriter.kt
@@ -0,0 +1,33 @@
+package ru.emkn.kotlin.sms.db
+
+import org.jetbrains.exposed.sql.Database
+import org.jetbrains.exposed.sql.deleteAll
+import ru.emkn.kotlin.sms.*
+
+/**
+ * Writes [ParticipantsList] to [database],
+ * overwriting all the existing data,
+ * to table that corresponds to [ParticipantsListTable].
+ */
+class ParticipantsListDbWriter(
+    private val database: Database,
+) {
+    // unsafe?
+    // should never throw anything
+    fun write(participantsList: ParticipantsList) {
+        return loggingTransaction(database) {
+            ParticipantsListTable.deleteAll()
+            participantsList.list.forEach { participant ->
+                ParticipantEntity.new(participant.id) {
+                    age = participant.age
+                    name = participant.name
+                    lastName = participant.lastName
+                    group = participant.group.label
+                    team = participant.team
+                    sportsCategory = participant.sportsCategory
+                    startingTime = participant.startingTime.toString()
+                }
+            }
+        }
+    }
+}
