cd198c6b19f56b425d6d0c29278ecd1cf02b96f3
Данила <duesna897@gmail.com>
2021-12-25 12:59:06 +0300

    Added a test demonstration in ParticipantsListTableTest.

--- src/main/kotlin/ru/emkn/kotlin/sms/db/ParticipantEntity.kt
+++ src/main/kotlin/ru/emkn/kotlin/sms/db/ParticipantEntity.kt
@@ -12,11 +12,11 @@ import ru.emkn.kotlin.sms.Participant
 class ParticipantEntity(id: EntityID<Int>) : IntEntity(id) {
     companion object : IntEntityClass<ParticipantEntity>(ParticipantsListTable)
     // id is assumed
-    val age by ParticipantsListTable.age
-    val name by ParticipantsListTable.name
-    val lastName by ParticipantsListTable.lastName
-    val group by ParticipantsListTable.group
-    val team by ParticipantsListTable.team
-    val sportsCategory by ParticipantsListTable.sportsCategory
-    val startingTime by ParticipantsListTable.startingTime
+    var age by ParticipantsListTable.age
+    var name by ParticipantsListTable.name
+    var lastName by ParticipantsListTable.lastName
+    var group by ParticipantsListTable.group
+    var team by ParticipantsListTable.team
+    var sportsCategory by ParticipantsListTable.sportsCategory
+    var startingTime by ParticipantsListTable.startingTime
 }
---
+++ src/test/kotlin/ru/emkn/kotlin/sms/db/ParticipantsListTableTest.kt
@@ -0,0 +1,49 @@
+package ru.emkn.kotlin.sms.db
+
+import org.jetbrains.exposed.sql.*
+import org.jetbrains.exposed.sql.transactions.transaction
+import kotlin.test.*
+
+internal class ParticipantsListTableTest {
+
+    private val testDBDir = "test-data/db/db-test-1"
+    private val testDBName = "sms-test"
+    private val testDBPath = "$testDBDir/$testDBName"
+
+    private fun ParticipantEntity.toDetailedString() = "$id. $name $lastName (group \"$group\", team \"$team\", sports category \"$sportsCategory\") starts at $startingTime"
+
+    @BeforeTest
+    fun initDB() {
+        Database.connect("jdbc:h2:./$testDBPath", driver = "org.h2.Driver")
+        transaction {
+            addLogger(StdOutSqlLogger)
+            SchemaUtils.create(ParticipantsListTable)
+            ParticipantEntity.new {
+                age = 25
+                name = "Alexey"
+                lastName = "Kolotilov"
+                group = "group1"
+                team = "team1"
+                sportsCategory = ""
+                startingTime = "00:00:00"
+            }
+        }
+    }
+
+    @Test
+    fun `sample participants list table test`() {
+        transaction {
+            ParticipantEntity.all().forEach {
+                println(it.toDetailedString())
+            }
+        }
+    }
+
+    @AfterTest
+    fun clearDB() {
+        transaction {
+            ParticipantsListTable.deleteAll()
+            exec(ParticipantsListTable.dropStatement().joinToString(" "))
+        }
+    }
+}
